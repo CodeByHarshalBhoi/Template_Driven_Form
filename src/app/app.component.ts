bbfcb73cef4c4aa905abaf4043ee6e7f5a37e07b
CodeByHarshalBhoi <prembhoi122513@hgmail.com>
2024-01-23 18:13:58 +0530

    Work with checkbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,14 @@ export class AppComponent {
     firstName:string=''
     lastName:string='';
     emailAddress:string='';
+    dob:string='';
+    gender:string='';
+    country:string='';
+    city:string='';
+    region:string='';
+    postal:string='';
+    userName:string=''
+    isAgree:boolean=false;
 
 
 genders=[
@@ -25,6 +33,8 @@ genders=[
 ]
 
 
+
+
   onFormSubmit() {
     console.log(this.form );
 
@@ -32,15 +42,90 @@ genders=[
     console.log(this.form.value.firstname);
     console.log(this.form.value.lastname);
     console.log(this.form.value.email);
-    console.log(this.form.value.country);
+    console.log(this.form.value.address.country);
 
 // METHOD-2 WE CAN ACCESS VALUE WITH CONTROLS ALSO
     console.log(this.form.controls['phone'].value);
     console.log(this.form.controls['dob'].value);
     console.log(this.form.controls['gender'].value);
-    console.log(this.form.controls['street1'].value);
-    console.log(this.form.controls['street2'].value);
+    console.log(this.form.controls['address'].value.street1);
+    console.log(this.form.controls['address'].value.street2);
+    console.log(this.form.value.username);
 
 
+
+    // ASIGNED VALUE FOR DISPLAY DATA
+    this.firstName = this.form.value.firstname;
+    this.emailAddress = this.form.value.email;
+    this.lastName = this.form.value.lastname;
+    this.dob = this.form.value.dob;
+    this.gender = this.form.value.gender;
+    this.country = this.form.value.address.country;
+    this.city = this.form.value.address.city;
+    this.region = this.form.value.address.region;
+    this.postal = this.form.value.address.postal;
+    this.userName = this.form.value.username;
+    this.isAgree= this.form.value.isAgree;
+
+
+this.form.reset();
+
+this.form.form.patchValue({
+  gender:'male',
+  address:{
+    country:'India'
+  }
+})
+
+
+
+    }
+
+    generateUserName(){
+      let username = '';
+      if(this.firstName.length>3){
+        username += this.firstName.slice(0,3);
+      }else{
+        username = this.firstName;
+      }
+
+      if(this.lastName.length>3){
+        username += this.lastName.slice(0,3);
+      }else{
+        username += this.lastName;
+      }
+
+      // console.log(this.dob)
+      let dateYear= new Date(this.form.controls['dob'].value);
+      console.log(dateYear);
+
+      username += dateYear.getFullYear();
+      username = username.toLowerCase();
+      console.log(username);
+      this.form.value.username = username;
+
+
+      // this.form.setValue({
+      //   firstname:this.form.value.firstname,
+      //   lastname:this.form.value.lastname,
+      //   email:this.form.value.email,
+      //   dob:this.form.value.dob,
+      //   phone:this.form.value.phone,
+      //   gender:this.form.value.gender,
+      //   username:username,
+      //   address:{
+      //     street1:this.form.value.address.street1,
+      //     street2:this.form.value.address.street2,
+      //     country:this.form.value.address.country,
+      //     city:this.form.value.address.city,
+      //     region:this.form.value.address.region,
+      //     postal:this.form.value.address.postal
+      //   }
+      // })
+
+
+      this.form.form.patchValue({
+        username:username
+      })
     }
 }
