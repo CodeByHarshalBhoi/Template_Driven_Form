3ed7e8e54f275eca5c150f8ed8f4c0c2eece921c
CodeByHarshalBhoi <prembhoi122513@hgmail.com>
2024-01-23 13:09:42 +0530

    Template Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Template_Driven_Form';
+
+
+  @ViewChild('registrationForm') form:NgForm;
+
+    firstName:string=''
+    lastName:string='';
+    emailAddress:string='';
+
+
+genders=[
+  {id:'check-male', value:'male', display:'Male'},
+  {id:'check-female', value:'female', display:'Female'},
+  {id:'check-other', value:'other', display:'Prefer not to say'},
+]
+
+
+  onFormSubmit() {
+    console.log(this.form );
+
+    // METHOD-1 WE CAN ACCESS VALUE WITH FORM.VALUE
+    console.log(this.form.value.firstname);
+    console.log(this.form.value.lastname);
+    console.log(this.form.value.email);
+    console.log(this.form.value.country);
+
+// METHOD-2 WE CAN ACCESS VALUE WITH CONTROLS ALSO
+    console.log(this.form.controls['phone'].value);
+    console.log(this.form.controls['dob'].value);
+    console.log(this.form.controls['gender'].value);
+    console.log(this.form.controls['street1'].value);
+    console.log(this.form.controls['street2'].value);
+
+
+    }
 }
